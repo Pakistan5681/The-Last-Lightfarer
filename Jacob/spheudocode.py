bc978b8090745b237fa64f83c353251c5cbc1862
INTELION11 <jacob.quezada@ucas-edu.net>
2026-04-21 16:43:33 +0000

    stared spheudo code

---
+++ Jacob/spheudocode.py
@@ -0,0 +1,18 @@
+# JQ 2nd Spheudo Code 16*9
+
+class knight:
+    
+
+
+
+
+
+
+
+
+
+# find grid size using (tilemap.size)
+# find blocked locations that are unaccesible
+# use monster location and player location to find clossest accessible tile near player
+# 
+
