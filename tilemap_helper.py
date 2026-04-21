df55018d4694cf66ee24a923077074762b47c840
Pakistan5681 <paxton.hall@ucas-edu.net>
2026-04-21 10:51:23 -0600

    Tile good

--- tilemap_helper.py
+++ tilemap_helper.py
@@ -1,9 +1,6 @@
 import pygame as py
 from random import randint, choice
-
-py.init()
-screen = py.display.set_mode((1920, 1152))
-clock = py.time.Clock()
+import math
 
 class Tile:
     def __init__(self, location, spritePath):
@@ -16,8 +13,30 @@ class Tile:
         outSprite = py.transform.scale(self.sprite, (128, 128))
         screen.blit(outSprite, self.rect.topleft)
 
-test = Tile((0, 0), "sprites//dirt_ground_5.png")
-test2 = Tile((1, 0), "sprites//dirt_ground_5.png")
+class Tilemap:
+    def __init__(self, size, tileCategory):
+        self.size = size
+        baseTiles = []
+        for x in range(size[0]):
+            for y in range(size[1]):
+                newTile = Tile((x, y), get_random_tile(tileCategory))
+                baseTiles.append(newTile)
+
+        self.tiles = baseTiles
+
+    def draw(self, screen):
+        for t in self.tiles: t.place(screen)
+
+class Player:
+    def __init__(self):
+        self.location = (5, 5)
+        self.sprite = py.image.load("sprites//MCfront//Idle.png").convert_alpha()
+        self.rect = self.sprite.get_rect()
+
+    def place(self, screen):
+        self.rect.topleft = ((self.location[0]) * 128, (self.location[1]) * 128)
+        outSprite = py.transform.scale(self.sprite, (128, 128))
+        screen.blit(outSprite, self.rect.topleft)
 
 def get_random_tile(category):  
     """
@@ -31,13 +50,22 @@ def get_random_tile(category):
             dirtList = ["sprites//dirt_ground_5.png", "sprites//dirt_ground_4.png", "sprites//dirt_ground_3.png", "sprites//dirt_ground_2.png", "sprites//dirt_ground_1.png"]
             return choice(dirtList)
         
-tiles = []
+def get_tile_mouse_pos():
+    mousepos = py.mouse.get_pos()
+    mouseX = math.floor(mousepos[0] / 128)
+    mouseY = math.floor(mousepos[1] / 128)
+    return (mouseX, mouseY)
 
-for j in range (9):
-    for i in range(16):
-        newTile = Tile((i, j), get_random_tile("dirt"))
-        tiles.append(newTile)
+py.init()
 
+size = (16, 9)
+
+screen = py.display.set_mode((size[0] * 128, size[1] * 128))
+clock = py.time.Clock()
+
+tilemap = Tilemap(size, "dirt")
+player = Player()
+      
 running = True
 while running:
     screen.fill((0, 0, 255))
@@ -45,11 +73,11 @@ while running:
     for event in py.event.get():
         if event.type == py.QUIT:
             running = False
+        elif event.type == py.MOUSEBUTTONDOWN:
+            player.location = get_tile_mouse_pos()
 
-    for i in tiles: i.place(screen)
-
-    test.place(screen)
-    test2.place(screen)
+    tilemap.draw(screen)
+    player.place(screen)
 
     py.display.flip()
 
