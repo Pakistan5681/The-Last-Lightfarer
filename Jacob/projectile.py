7310e0a060a9346d033d782007fcbbca8e02439c
INTELION11 <jacob.quezada@ucas-edu.net>
2026-04-27 16:07:55 +0000

    started working on bishop

---
+++ Jacob/projectile.py
@@ -0,0 +1,26 @@
+import pygame as py
+import math
+class Projectile:
+    def __init__(self, spritePath, startpos, targetpos, speed, damage):
+        self.sprite = py.transform.rotate(py.image.load(spritePath).convert_alpha(), 45)
+        self.x = float(startpos[0] * 128)
+        self.y = float(startpos[1] * 128)
+        self.targetx = float(targetpos[0] * 128)
+        self.targety = float(targetpos[1] * 128)
+        self.speed = speed
+        self.damage = damage
+        self.alive = True
+
+    def draw(self, screen, player):
+        horizontal_distance = self.targetx - self.x
+        vertical_distance = self.targety - self.y
+        total_distance = math.sqrt(horizontal_distance**2 + vertical_distance**2)
+
+        if total_distance <= self.speed:
+            player.health -= self.damage
+            self.alive = False
+        else:
+            self.x += (horizontal_distance / total_distance) * self.speed
+            self.y += (vertical_distance / total_distance) * self.speed
+            scaled_sprite = py.transform.scale(self.sprite, (64, 64))
+            screen.blit(scaled_sprite, (self.x, self.y))
--- Jacob/spheudocode.py
+++ Jacob/spheudocode.py
@@ -2,7 +2,7 @@
 import pygame as py
 from random import randint, choice
 import math
-
+from Jacob.projectile import Projectile
 class Weapon:
     def __init__(self, name, damage, type, range):
         self.name = name
@@ -10,19 +10,19 @@ class Weapon:
         self.type = type
         self.range = range
 
-    def get_attack_squares(self, enemeypos):
+    def get_attack_squares(self, enemeypos,occupied=[]):
         squares = []
         psquares=[]
         if self.type == "pawn":
             for x in range(enemeypos[0] - self.range, enemeypos[0] + self.range):
                 for y in range(enemeypos[1] - self.range, enemeypos[1] + self.range):
-                    if (x, y) != enemeypos: squares.append((x, y))
+                    if (x, y) != enemeypos and (x, y) not in occupied: squares.append((x, y))
         elif self.type == "rook": 
             for i in range(self.range * 3):
-                squares.append((enemeypos[0], enemeypos[1] + i))
-                squares.append((enemeypos[0], enemeypos[1] - i))
-                squares.append((enemeypos[0] + i, enemeypos[1]))
-                squares.append((enemeypos[0] - i, enemeypos[1]))
+                if (enemeypos[0], enemeypos[1] + i) not in occupied: squares.append((enemeypos[0], enemeypos[1] + i))
+                if (enemeypos[0], enemeypos[1] - i) not in occupied: squares.append((enemeypos[0], enemeypos[1] - i))
+                if (enemeypos[0] + i, enemeypos[1]) not in occupied: squares.append((enemeypos[0] + i, enemeypos[1]))
+                if (enemeypos[0] - i, enemeypos[1]) not in occupied: squares.append((enemeypos[0] - i, enemeypos[1]))
         elif self.type == "bishop": 
             for i in range(self.range * 3):
                 psquares.append((enemeypos[0] + i, enemeypos[1] + i))
@@ -31,28 +31,32 @@ class Weapon:
                 psquares.append((enemeypos[0] - i, enemeypos[1] + i))
             for x in range(enemeypos[0] - self.range, enemeypos[0] + self.range):
                 for y in range(enemeypos[1] - self.range, enemeypos[1] + self.range):
-                    if (x, y) != enemeypos: squares.append((x, y)) 
+                    if (x, y) != enemeypos and (x, y) not in occupied: squares.append((x, y))
         elif self.type == "Knight":
-            for i in range(self.range * 15):
-                squares.append((enemeypos[0] + i, enemeypos[1] + 1))
-                squares.append((enemeypos[0] + i, enemeypos[1] - 1))
-                squares.append((enemeypos[0] - i, enemeypos[1] - 1))
-                squares.append((enemeypos[0] - i, enemeypos[1] + 1))
-                squares.append((enemeypos[0] -1, enemeypos[1] + i))
-                squares.append((enemeypos[0] +1, enemeypos[1] + i))
-                squares.append((enemeypos[0] -1, enemeypos[1] - i))
-                squares.append((enemeypos[0] +1, enemeypos[1] - i))
-        
+            knight_moves = [
+                (enemeypos[0] + 2, enemeypos[1] + 1),
+                (enemeypos[0] + 2, enemeypos[1] - 1),
+                (enemeypos[0] - 2, enemeypos[1] + 1),
+                (enemeypos[0] - 2, enemeypos[1] - 1),
+                (enemeypos[0] + 1, enemeypos[1] + 2),
+                (enemeypos[0] - 1, enemeypos[1] + 2),
+                (enemeypos[0] + 1, enemeypos[1] - 2),
+                (enemeypos[0] - 1, enemeypos[1] - 2),
+            ]
+            for sq in knight_moves:
+                if sq not in occupied:
+                    squares.append(sq)
+            return squares
         return squares         
-        
-
 class Monster:
-    def __init__(self, spriteImage, location, weapon):
+    def __init__(self, spriteImage, location, weapon, damage, projectile_sprite="sprites/sylf/sylphwing-spell.png"):
         self.location = location
         self.sprite = py.image.load(spriteImage).convert_alpha()
         self.rect = self.sprite.get_rect()
         self.health = 100
         self.weapon = weapon
+        self.damage = damage
+        self.projectile_sprite =projectile_sprite 
 
     def place(self, screen):
         print(self.location)
@@ -63,7 +67,23 @@ class Monster:
     def move(self, playerpos, screenSize):
         squares = self.weapon.get_attack_squares(self.location)
         if self.weapon.type == "bishop":
-            print()
+            best_square = self.location
+            min_diagonal_dist = float('inf')
+    
+            for p in squares:
+                pdx = abs(p[0] - playerpos[0])
+                pdy = abs(p[1] - playerpos[1])
+                diagonal_dist = abs(pdx - pdy)
+                if diagonal_dist < min_diagonal_dist:
+                    min_diagonal_dist = diagonal_dist
+                    best_square = p
+    
+            self.location = best_square
+            bdx = abs(self.location[0] - playerpos[0])
+            bdy = abs(self.location[1] - playerpos[1])
+            if bdx == bdy:
+                return Projectile(self.projectile_sprite, self.location, playerpos, speed=5, damage=self.damage)
+            return 0
         target = playerpos
         closest_point = self.location
         min_dist = float('inf') # Start with infinity
@@ -75,6 +95,11 @@ class Monster:
                 closest_point = p
 
         self.location = closest_point   
+        dx = abs(self.location[0] - playerpos[0])
+        dy = abs(self.location[1] - playerpos[1])
+        if dx <= 1 and dy <= 1 and (dx + dy) > 0:
+                return self.damage
+        return 0
 
 # Create class monster
 #   initialize with:
@@ -87,3 +112,15 @@ class Monster:
 # if monster type is knight, graph all squares in an x shape jump pattern
 # if monster type is rook, graph all squares in an + shape
 # if monster type is pawn, graph all squares in an square shape
+#        if self.type == "bishop":
+#            for p in squares:
+#                out = []
+#                out.append(DisplaySprite("sprites//Indicators//attack_indicator.png", i))
+#                dist = math.sqrt((p[0] - target[0])**2 + (p[1] - target[1])**2)
+#                if dist < min_dist:
+#                    min_dist = dist
+#                    closest_point = p
+#            self.location = closest_point
+#            if self.weapon.type == "bishop":
+    # Find which of the 4 diagonals the player is on relative to the monster
+    # A square is on the same diagonal if dx == dy
