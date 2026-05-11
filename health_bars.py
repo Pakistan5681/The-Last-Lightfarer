9227857c43dedf81cc982a65b75372639d2acfe0
Pakistan5681 <paxton.hall@ucas-edu.net>
2026-05-11 10:46:04 -0600

    HUZZAH

--- health_bars.py
+++ health_bars.py
@@ -14,6 +14,7 @@ class PRbar():
         self.image = py.Surface((player_max * 2, 40))    
 
     def draw(self, screen, pHealth):
+        if pHealth < 0: pHealth = 0
         newImage = py.transform.scale(self.image, (pHealth * 2, 40))
         self.image.fill((255, 0, 0))
         newRect = newImage.get_rect(topleft=(50, 50))
