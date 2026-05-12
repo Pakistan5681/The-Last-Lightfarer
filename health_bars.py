b5736871cd0103087a4a459e9ca47a3a316298c2
INTELION11 <jacob.quezada@ucas-edu.net>
2026-05-12 16:35:50 +0000

    think it works now monster health bar

--- health_bars.py
+++ health_bars.py
@@ -22,3 +22,32 @@ class PRbar():
 
 
 
+class MPhealth():
+    def __init__(self,mLocation):
+        self.sprite = py.image.load("sprites//GUI//health_bar.png").convert_alpha()
+        self.x = float(mLocation[0] * 128)
+        self.y = float(mLocation[1] * 128) 
+
+    def draw(self, screen):
+        newImage = py.transform.scale(self.sprite, (200, 400))
+        up = self.y + 28
+        newRect = newImage.get_rect(topleft=(self.x,up))
+        screen.blit(newImage, newRect)
+
+
+class MPRbar():
+    def __init__(self, player_max,mLocation):
+        self.image = py.Surface((player_max * 2, 40))
+        self.x = float(mLocation[0] * 128)
+        self.y = float(mLocation[1] * 128)   
+
+    def draw(self, screen, mHealth):
+        location = ()
+        if mHealth < 0:mpHealth = 0
+        newImage = py.transform.scale(self.image, (mHealth * 2, 40))
+        self.image.fill((255, 0, 0))
+        up = self.y + 28
+        newRect = newImage.get_rect(topleft=(self.x,up))
+        screen.blit(newImage, newRect)
+
+
